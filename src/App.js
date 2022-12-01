d6b9589a358fed21ce0e3e83e03ef43b88bdd671
unknown <pallavikate09@gmail.com>
2022-12-02 01:14:55 +0530

    After Adding hover and bookmark effect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import {useState,useEffect} from 'react'
 import axios from 'axios';
-import { ModalHover } from 'react-modal-hover'
 import {Scrollbars} from 'react-custom-scrollbars'
 
 function App() {
   const [getsearch,setGetSearch]= useState("")
   const [searchedInput,setSearchedInput] = useState("")
  const [search,setSearch] = useState([])
+ console.log(search)
  const [hover,setHover] = useState(false)
  const [hoverResult,setHoverResult] = useState([])
+ const [bookmargkImages,setBookmarkImages]=useState("")
+ console.log(bookmargkImages)
   const key = 'PsyYpkRlysxfXQZ99-bZFf5msPHyopRoa5KGrC94cc0'
 
  useEffect(()=>{
@@ -19,6 +21,7 @@ function App() {
         console.log(searchedInput)
  const Result = await axios.get(`https://api.unsplash.com/search/photos?query=${searchedInput}&client_id=${key}`)
  setSearch(Result.data.results)
+ setBookmarkImages("")
       }  
    }
   catch(error){
@@ -36,7 +39,7 @@ function App() {
   setSearchedInput(getsearch)
  }
 
- const hoverEffect = ()=>{
+ const hoverEffect = (e)=>{
      setHover(true)
  }
 
@@ -44,12 +47,19 @@ function App() {
       setHoverResult([...hoverResult,e.target.id])
   }
 
+  const showBookMarkImages = ()=>{
+    setBookmarkImages("true")
+    setSearch(hoverResult)
+  }
+
+
   return (
     <div className="App">
       <div className="mainContiner">
       <div>
         <h1>React Photo Search</h1>
-        <button class="btn btn-primary" style={{marginLeft:"10px"}}>Bookmark</button>
+        <button class="btn btn-primary" style={{marginLeft:"10px"}}
+        onClick={showBookMarkImages}>Bookmark</button>
       </div>
       <div>
         <input type="search" onChange={searchInput} class="form-control" placeholder='search'/>
@@ -61,9 +71,16 @@ function App() {
         search.map((element)=>{
         return(
           <>
-           <div style={{display:"grid",gridTemplateRows:"90% 10%"}}>
-           <img src={element.urls.regular} style={{width:"100%",height:"100%",padding:"10px"}} hover={()=>{console.log('hello')}} onMouseOver={hoverEffect}/>
-           {hover?<p style={{color:"blue"}} id={element.urls.regular}  onClick={captured}>Bookmark</p>:""}
+           <div className='hoveroverme' style={{display:"grid",gridTemplateRows:"90% 10%"}}>
+            {
+              !bookmargkImages ?
+              <>
+              <img src={element.urls.regular} style={{width:"100%",height:"100%",padding:"10px"}} hover={()=>{console.log('hello')}} onMouseOver={hoverEffect}/> 
+              <div style={{marginTop:"300px",marginLeft:"100px"}} onClick={captured}><p style={{marginLeft:"30%",color:"blue",fontWeight:"600",fontSize:"20px"}} id={element.urls.regular}> Bookmark</p></div>
+              </> :
+              <img src={element} style={{width:"100%",height:"100%",padding:"10px"}} hover={()=>{console.log('hello')}} onMouseOver={hoverEffect}/>
+              
+            }
            </div>
           </>
          )
